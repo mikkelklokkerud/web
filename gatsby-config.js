5ca69f60f0f4cbecf299587e85db6ae744aae8ca
Mikkel Eriksen Klokkerud <mikkelek93@gmail.com>
2020-05-02 23:46:58 +0200

    Add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,6 +29,13 @@ module.exports = {
       }
     },
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // replace "UA-XXXXXXXXX-X" with your own Tracking ID
+        trackingId: "UA-163172180-1",
+      },
+    },
+    {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
         fonts: [
@@ -59,3 +66,4 @@ module.exports = {
     // `gatsby-plugin-offline`,
   ],
 }
+
