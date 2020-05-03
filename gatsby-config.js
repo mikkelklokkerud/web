dc3228da4f785413feb40509346b06153333013f
Mikkel Eriksen Klokkerud <mikkelek93@gmail.com>
2020-05-03 19:00:24 +0200

    Updated favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -63,12 +63,10 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon-32x32.png`,
+        legacy: false,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
 
