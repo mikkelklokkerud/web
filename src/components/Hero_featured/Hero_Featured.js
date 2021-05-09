87e1679283c7d0d3fa3796ff785204ce28a53219
Mikkel Eriksen Klokkerud <mikkel@mac.home>
2021-05-09 13:10:09 +0200

    tailwind

--- src/components/Hero_featured/Hero_Featured.js
+++ src/components/Hero_featured/Hero_Featured.js
@@ -46,60 +46,19 @@ class Hero_Featured extends React.Component {
 
             <p className="">Gatsby & Next + Headless CMS</p>
           </div>
-          <div className="border-l-2 border-black pl-10 lg:pr-10 py-16">
+          <div className="border-l-2 border-black pl-10 lg:pr-10 py-16 font-normal">
             <Link key={key} to={`/blog/${slug}`}>
               <div className="">
-                <h4 className="">Featured {featuredType}</h4>
-                <h2 className="text-2xl font-bold leading-tight mb-1">{title}</h2>
-                <small className="mt-3 mb-2 block">{publishedDate}</small>
-                <p className="font-normal">{shortDescription}</p>
+                <div className="mb-5">Featured {featuredType}</div>
+                <h2 className="text-2xl font-bold leading-tight mb-1">
+                  {title}
+                </h2>
+                <small className="mt-3 mb-3 block">{publishedDate}</small>
+                <p>{shortDescription}</p>
               </div>
             </Link>
           </div>
         </div>
-        {/* <div className={styles.container}>
-          <div className={styles.left}>
-          
-         
-              <StaticQuery
-                query={graphql`
-                  query LogoQuery {
-                    file(name: { eq: "logo-white-bg" }) {
-                      childImageSharp {
-                        fluid {
-                          ...GatsbyImageSharpFluid_tracedSVG
-                        }
-                      }
-                    }
-                  }
-                `}
-                render={data => (
-                <Image fluid={data.file.childImageSharp.fluid}                 
-                  style={{ width: "70%", marginLeft: "auto", marginRight: "auto" }}
-                  alt="MikkelCodes-logo"
-
-                  />
-                 
-                )}
-              />
-          
-
-            <p className={styles.tagline}>
-              Gatsby or Next + Headless CMS
-            </p>
-          </div>
-          <div className={styles.right}>
-            <Link key={key} to={`/blog/${slug}`}>
-              <div className={styles.content}>
-                <h1 className={styles.page}>{page}</h1>
-                <h4 className={styles.subtitle}>Featured {featuredType}</h4>
-                <h2 className={styles.title}>{title}</h2>
-                <small className={styles.date}>{publishedDate}</small>
-                <p className={styles.excerpt}>{shortDescription}</p>
-              </div>
-            </Link>
-          </div>
-        </div> */}
       </div>
     )
   }
